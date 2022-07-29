b8c915007a4067db42e41a947c5b6aab2b4f7f87
dbushhf12 <53022296+dbushhf12@users.noreply.github.com>
2022-07-29 14:26:54 -0400

    Create 4.5.6_French.py

---
+++ 4.5.6_French.py
@@ -0,0 +1,58 @@
+#Do not change the line of code below. It's at the top of
+#the file to ensure that it runs before any of your code.
+#You will be able to access french_dict from inside your
+#function.
+french_dict = {"me": "moi", "hello": "bonjour", 
+               "goodbye": "au revoir", "cat": "chat", 
+               "dog": "chien", "and": "et"}
+
+#Write a function called french2eng that takes in one string
+#parameter called sentence. french2eng should look at each
+#word in the sentence and translate it into French if it is
+#found in the dictionary, french_dict. If a word is not found
+#in the dictionary, do not translate it: use the original
+#word. Then, the function should return a string of the
+#translated sentence.
+#
+#You may assume that the sentence you're translating has no
+#punctuation. However, you should convert it to lower case
+#before translating.
+#
+#For example:
+#
+#  french2eng("Hello it's me") -> "bonjour it's moi"
+#
+#Hint: Use .split() to get a list of strings representing
+#each word in the string, then use ' '.join to merge the
+#translated list back into one string.
+#
+#Hint 2: Remember, lists are mutable, so we can change
+#individual items in the list. However, to change an item
+#in a list, we must change it using its index. We can
+#write lines like my_words[1] = new_word.
+#
+#Hint 3: If you're stuck, try breaking it down into small
+#parts. How do you access an item from a list? How do you
+#look up a key in a dictionary? How do you change the
+#value of an item in a list? How do you check if a key is
+#in the dictionary?
+
+#Write your function here!
+def french2eng(sentence):
+    lower_str = ""
+    french_str = ""
+    lower_str = sentence.lower()
+    lower_str = lower_str.split()
+    for i in range(len(lower_str)):
+        if lower_str[i] in french_dict:
+            lower_str[i] = french_dict[lower_str[i]]
+    french_str = " ".join(lower_str)
+    return french_str
+
+#Below are some lines of code that will test your function.
+#You can change the value of the variable(s) to test your
+#function with different inputs.
+#
+#If your function works correctly, this will originally
+#print: bonjour it's moi
+print(french2eng("Hello it's me"))
